6b4f54dbf1bae68b0613f3ffc4d698b422a581e3
Vito Genovese <genovese.work@protonmail.com>
2024-09-19 19:45:30 +0200

    arrays init

--- Arrays/Arrays.ino
+++ Arrays/Arrays.ino
@@ -1,5 +1,11 @@
+int nums[4] = {1,2,3,4}; // -> []
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  for (int i = 0; i < 4; i ++) {
+    Serial.println(nums[i]);
+  }
 
 }
 
