2064436afa4d9ce0544bddd43bd1260a6d09496b
kuga <kuga@kugas-MacBook-Air.local>
2024-10-18 09:45:36 +0200

    pot

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -8,16 +8,13 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
-  servo1.attach(5);
+  servo1.attach(5, 500, 2500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  val = analogRead(A1)/4; // -> analogRead() [0,1023]/4 -> [0,255]
-  analogWrite(LED, val); // -> analogWrite() [0,255]
-  delay(100);
-  servo1.write(0, 0, 1023, 0, 180);
-  delay(1000);
-  servo1.write(180, 0, 1023, 0, 180);
-  delay(1000);
+  val = analogRead(A1); // -> analogRead() [0,1023]/4 -> [0,255]
+  //analogWrite(LED, val); // -> analogWrite() [0,255]
+  val = map(val, 0, 1023, 0, 180);
+  servo1.write(val);
 }
